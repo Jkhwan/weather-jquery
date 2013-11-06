e918822c08e6d3b97c64709b7ad413a93c3a4fbb
Jason Wan <jwan823@gmail.com>
2013-11-06 15:59:02 -0800

    Beta version

--- js/app.js
+++ js/app.js
@@ -1,8 +1,67 @@
 $(function() {
 	$('form#searchCity').submit(function() {
+
+		// grab city from input box
 		var cityInputBox = $('input#cityInput');
 		var city = cityInputBox.val();
+		// remove all list items
+		$('h1').text("Weather Forecast");
+		$('ul#citiesList li').remove();
+		$('.row.weather div.dailyForecast').remove();
+		$('div.row.weather').hide();
+		$('ul#citiesList').show();
+
+		var url = "http://autocomplete.wunderground.com/aq?query=" + city + "&format=jsonp&cb=?";
+		var cityLookupSuccess = function(data) {
+			$.each(data.RESULTS, function(index, value) {
+				var li = $('<li class="list-group-item" data-zmw="' + value.zmw + '" data-city="' + value.name + '">' + value.name + '</li>').hide();
+				$('ul#citiesList').append(li);
+			});
+
+			// creates fadeIn effects
+			var fadeItem = function() {
+				$('ul#citiesList li:hidden:first').fadeIn(50);
+				$('ul#citiesList li:hidden:first').fadeIn(50, fadeItem);
+			}
+			fadeItem();
+		}
+		$.getJSON(url, cityLookupSuccess);
+
+		// clear input box
 		cityInputBox.val("");
-		console.log(city);
+		return false;
+	});
+
+	$('ul#citiesList').on('click', 'li', function() {
+		var zmw = $(this).data("zmw");
+		var city = $(this).data('city');
+		var key = '3c8121ac154d80de';
+		var cityWeatherURL = "http://api.wunderground.com/api/" + key + "/forecast/q/zmw:" + zmw +".json";
+		var cityWeatherSuccess = function(data) {
+			console.log(data);
+			$('ul#citiesList li').remove();
+			$('ul#citiesList').hide();
+			var weatherDiv = $('div.row.weather');
+			for(var i = 0; i < 4; i++) {
+				var dailyDiv = $('<div class="dailyForecast col-xs-3"></div>');
+				var weatherData = data.forecast.simpleforecast.forecastday[i];
+				dailyDiv.append($('<div>' + weatherData.date.weekday + ', ' + weatherData.date.monthname_short 
+					+ ' ' + weatherData.date.day + ' ' + '</div>'));
+				dailyDiv.append($('<img src="' + weatherData.icon_url + '"><span class="temperature">' + weatherData.high.celsius + '&deg;C</span>'))
+				dailyDiv.append($('<div>' + weatherData.conditions + '</div><br />'));
+				dailyDiv.append($('<div class="simple">P.O.P: ' + weatherData.pop + '%</div>'));
+				dailyDiv.append($('<div class="simple">Rain: ' + weatherData.qpf_allday.mm + ' mm</div>'));
+				dailyDiv.append($('<div class="simple">Humidity: ' + weatherData.avehumidity + '%</div>'));
+				weatherDiv.append(dailyDiv);
+			}
+			$('h1').text(city);	
+			weatherDiv.show();
+		};
+		$.ajax({
+			url: cityWeatherURL, 
+			dataType: 'jsonp',
+			success: cityWeatherSuccess
+		});
+
 	});
 });
